aebe204b5529ad198b48ee792b6ed03cd58d55aa
Justin Diaz <justin.diaz@howardcc.edu>
2023-03-18 02:50:51 -0400

    started exercise: _18-Palindromes (Recursion)

---
+++ _18-Palindromes/src/Palindrome.java
@@ -0,0 +1,18 @@
+/**
+ * Palindrome.java
+ * Determines whether a string entered by the user is a palindrome
+ *
+ * @author Justin Diaz
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        input.close(); // close input stream
+    } // end method main
+} // end class Palindrome
