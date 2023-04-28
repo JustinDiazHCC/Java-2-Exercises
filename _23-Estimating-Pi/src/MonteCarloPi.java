3970faa132e6f3a3af1ab3866c00ce1a3c336be9
Justin Diaz <justin.diaz@howardcc.edu>
2023-04-27 23:04:14 -0400

    started monte carlo pi estimation

---
+++ _23-Estimating-Pi/src/MonteCarloPi.java
@@ -0,0 +1,64 @@
+package edu.howardcc.cmsy167;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
+
+/**
+ * Driver for Monte Carlo Pi estimation
+ */
+public class MonteCarloPi {
+
+    public static final int NUM_THREADS = 1;
+    public static final int NUM_ITERATIONS = 100_000_000;
+
+    public static void main(String[] args) {
+
+        System.out.printf("Available Processors: %s\n", Runtime.getRuntime().availableProcessors());
+
+        int iterationsPerThread = NUM_ITERATIONS/NUM_THREADS;
+        PiCalculator piCalculator = new PiCalculator(iterationsPerThread);
+
+        Instant startTime = Instant.now();
+        piCalculator.getPiEstimate();
+        Instant finishTime = Instant.now();
+
+        long timeInMilliseconds = Duration.between(startTime, finishTime).toMillis();
+        System.out.printf("Approximation of Pi using %d iterations and %d thread(s) completed in %d milliseconds: %f\n",
+                NUM_ITERATIONS, NUM_THREADS, timeInMilliseconds, piCalculator.getPiEstimate());
+
+    }
+
+    /**
+     * Uses a Monte Carlo method to estimate Pi
+     */
+    public static class PiCalculator {
+
+        private final int iterations;
+
+        public PiCalculator(int iterations) {
+            this.iterations = iterations;
+        }
+
+        public double getPiEstimate() {
+
+                final Random random = new Random();
+
+                int circlePoints = 0;
+                int squarePoints = 0;
+
+                for (int i = 0; i < iterations; i++) {
+                    double x = random.nextDouble();
+                    double y = random.nextDouble();
+                    double d = x*x + y*y;
+
+                    if (d <= 1) {
+                        circlePoints++;
+                    }
+                    squarePoints++;
+                }
+
+            return 4.0 * ((double)circlePoints / (double)squarePoints);
+        }
+    }
+}
