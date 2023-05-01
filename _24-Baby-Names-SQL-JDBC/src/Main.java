89e3c57747946343b22e0af24c6146533c2cc6ba
JustinDiazHCC <justin.diaz@howardcc.edu>
2023-05-01 00:17:46 -0400

    imported apache derby, finished part 1, added part 1 database query .txt file and query number 6 .png

---
+++ _24-Baby-Names-SQL-JDBC/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main.java
+ *
+ * @author Justin Diaz
+ * @version 1.0
+ */
+public class Main {
+    public static void main (String[] args) {
+        DBConnection connectTest = new DBConnection();
+    }
+}
